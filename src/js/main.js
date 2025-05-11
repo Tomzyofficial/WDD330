7e610a784c7653252a3c3f4c99446b198ec2397e
Tomzyofficial <tomzyofficial54@gmail.com>
2025-05-12 00:00:31 +0100

    Revert "https://github.com/Tomzyofficial/WDD330"
    This reverts commit 9640d4d7b54465b8b1d5186f48a1c96ff10cd4a7.

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
-
-const dataSource = new ProductData("tents");
-
-const ele = document.querySelector(".product-list");
-
-const products = new ProductList("Tents", dataSource, ele);
-products.init();
