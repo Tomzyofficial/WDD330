9d78a11cb7079348fb661478069fb2bb739ddf16
Slapicola <slapicola@byupathway.edu>
2025-05-17 18:25:40 -0400

    Our Week 2 Team Activity work

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,39 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId)
+        this.renderProductDetails()
+        document.getElementById('addToCart')
+            .addEventListener('click', this.addProductToCart.bind(this));
+
+    }
+    addProductToCart(product) {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+    }
+    renderProductDetails() {
+        htmlTemplate(this.product)
+    }
+}
+  
+function htmlTemplate(product) {
+    document.querySelector("h2").textContent = product.Brand.Name
+    document.querySelector("h3").textContent = product.NameWithoutBrand
+    const productImage = document.getElementById("productImage")
+    productImage.src = product.Image
+    productImage.alt = product.NameWithoutBrand
+
+    document.querySelector(".product-card__price").textContent = product.FinalPrice
+
+    document.querySelector(".product__color").textContent = product.Colors[0].ColorName
+    document.querySelector(".product__description").innerHTMl = product.DesciptionHtmlSimple
+
+    document.querySelector("#addToCart").dataset.id = product.Id
+}
