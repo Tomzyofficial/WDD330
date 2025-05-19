407fdcb01a068d0eea4cb40f801c3b63d0fa4d71
L33hy <lehidaniel99@gmail.com>
2025-05-19 21:10:40 +0100

    weekly activity

--- src/js/product.js
+++ src/js/product.js
@@ -6,7 +6,7 @@ const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
-  setLocalStorage("so-cart", cartItems) ;
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -15,4 +15,6 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
