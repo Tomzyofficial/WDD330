3ef35ef3e2a3ce9cd88f2b93dbe56a5d4d6858b7
Slapicola <slapicola@byupathway.edu>
2025-05-23 13:19:36 -0400

    In progress week3 individual activity

--- src/js/ProductList.js
+++ src/js/ProductList.js
@@ -4,7 +4,7 @@ function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}">
+        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -18,8 +18,9 @@ export default class ProductList {
     this.listElement = listElement;
   }
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     this.renderList(list);
+    document.querySelector(".product").textContent = this.category;
   }
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
